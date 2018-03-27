e9ab20a9f4c0af9dd7f2e7862f3539fda5607160
Titch Deane <titch@hotmail.co.uk>
2018-03-27 13:01:20 -0700

    Integrate stripe and heroku

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -2,7 +2,7 @@ class ChargesController < ApplicationController
 
   def new
     @stripe_btn_data = {
-      key: "pk_test_yR1QLRLToKNdePIU80eLl1gA",
+      key: ENV['STRIPE_PUBLISHABLE_KEY'],
       description: "Employ Employer Membership - #{current_user.email}",
       amount: 1000
     }
